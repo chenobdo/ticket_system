92b793cf60dcebfcb88f3f814b7282ef76acdb6b
Gabriel <obdo.briel@gmail.com>
2017-07-03 18:05:50 +0800

    feat locale admin-ticket-show

--- resources/views/admin/tickets/show.blade.php
+++ resources/views/admin/tickets/show.blade.php
@@ -6,12 +6,12 @@
 
     <section class="content-header">
       <h1>
-        Admin
-        <small>Tickets</small>
+        后台
+        <small>工单</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> Admin</a></li>
-        <li class="active">Tickets</li>
+        <li><a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> 后台</a></li>
+        <li class="active">工单</li>
       </ol>
     </section>
 
@@ -33,23 +33,23 @@
                             
                                     {!! csrf_field() !!}      
                                     
-                                    <p>Message: <i><h4>{!! $ticket->message !!}</h4></i></p>
-                                    <p>Category: {{ Form::select('category',$categories,$ticket->category_id,['class'=>'form-control'])}}</p>
-                                    <p>Status: {{ Form::select('status',$statuses,$ticket->status_id,['class'=>'form-control'])}}</p>
-                                    <p>Priority:{{ Form::select('priority',$prioritys,$ticket->priority_id,['class'=>'form-control'])}}</p>
-                                    <p>Created: {{ $ticket->created_at->diffForHumans() }}</p>
-                                    <p>Last Updated: {{ $ticket->updated_at->diffForHumans() }}</p>
+                                    <p>内容: <i><h4>{!! $ticket->message !!}</h4></i></p>
+                                    <p>类型: {{ Form::select('category',$categories,$ticket->category_id,['class'=>'form-control'])}}</p>
+                                    <p>状态: {{ Form::select('status',$statuses,$ticket->status_id,['class'=>'form-control'])}}</p>
+                                    <p>优先级:{{ Form::select('priority',$prioritys,$ticket->priority_id,['class'=>'form-control'])}}</p>
+                                    <p>创建时间: {{ $ticket->created_at->diffForHumans() }}</p>
+                                    <p>最进更新: {{ $ticket->updated_at->diffForHumans() }}</p>
                                     
                         <div class="row">
                             <div class="col-md-6">
-                                <button type="submit" class="btn bg-purple"><i class="fa fa-btn fa-ticket"></i> Update Ticket</button>   
+                                <button type="submit" class="btn bg-purple"><i class="fa fa-btn fa-ticket"></i> 更新工单</button>
                                {!! Form::close() !!}   
                                
                             </div>
                             
                             <div class="col-md-6">               
                                     {!! Form::open(['method' => 'DELETE','route' => ['managetickets.destroy', $ticket->ticket_id],'style'=>'display:inline', 'class'=>'deletetickets']) !!}
-                                    {{ Form::button('<i class="fa fa-remove"></i> Delete', array('class'=>'btn btn-danger pull-right', 'type'=>'submit')) }}
+                                    {{ Form::button('<i class="fa fa-remove"></i> 删除', array('class'=>'btn btn-danger pull-right', 'type'=>'submit')) }}
                                     {!! Form::close() !!}                                
                             </div>
                         </div>    
@@ -60,13 +60,13 @@
     <div class="col-md-7">
           <div class="box box-default">
             <div class="box-header with-border">
-              <h3 class="box-title"><i class="fa fa-comments"> Comments</i></h3>
+              <h3 class="box-title"><i class="fa fa-comments"> 评论</i></h3>
             </div>
 
            <div class="box-body chat" id="chat-box">
 
                    @if ($comments->isEmpty())
-                        <p>There is no comments yet</p>
+                        <p>这里仍然没有评论</p>
                     @else
 
             @foreach ($comments as $comment)   
@@ -95,7 +95,7 @@
                 @if ($ticket->status_id === 3)
                     <form action="{{ url('reopen/' . $ticket->ticket_id) }}" method="POST" class="form">
                       {!! csrf_field() !!}
-                         <button type="submit" class="btn bg-purple">Reopen</button>
+                         <button type="submit" class="btn bg-purple">重开</button>
                     </form>            
                 @else
             
@@ -114,7 +114,7 @@
                                         </div>
                                 
                                         <div class="form-group">
-                                            <button type="submit" class="btn bg-purple">Publish comment</button>
+                                            <button type="submit" class="btn bg-purple">发表评论</button>
                                         </div>
                                     </form>
                         </div>
