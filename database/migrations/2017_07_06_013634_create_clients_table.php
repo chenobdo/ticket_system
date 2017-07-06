ed41dd72daacfe5e299dbcb4f882091f71e9c2de
Gabriel <obdo.briel@gmail.com>
2017-07-06 10:02:42 +0800

    op clients table migrations

--- database/migrations/2017_07_06_013634_create_clients_table.php
+++ database/migrations/2017_07_06_013634_create_clients_table.php
@@ -18,6 +18,11 @@ class CreateClientsTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->char('contractno', 10)->default('')->comment('合同编号');
+            $table->smallInteger('is_continue')->unsigned()->default(1)->comment('1-首次投资；2-非首次；3-续投');
+            $table->char('client', 10)->default('')->comment('出借人姓名');
+            $table->char('client', 18)->default('')->comment('出借人身份证ID');
+            $table->char('gender', 1)->default('M')->comment('M-男，F-女');
+            $table->smallInteger('bond_type')->unsigned()->default(1)->comment('债券接受方式（1-纸质；2-电子邮件；3-两者皆收；4-无需填写）');
             $table->string('name');
             $table->string('airline');
             $table->timestamps();
