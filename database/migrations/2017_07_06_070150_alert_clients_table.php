101895e93749c290aeeba877cf730559369b5620
Gabriel <obdo.briel@gmail.com>
2017-07-06 15:10:08 +0800

    feat complete alert_clients_table migration

--- database/migrations/2017_07_06_070150_alert_clients_table.php
+++ database/migrations/2017_07_06_070150_alert_clients_table.php
@@ -13,7 +13,10 @@ class AlertClientsTable extends Migration
     public function up()
     {
         Schema::table('clients', function (Blueprint $table) {
-            //
+            $table->decimal('FTA', 10, 2)->default(0)->comment('折标金额')->change();
+            $table->decimal('loan_amount', 10, 2)->default(0)->comment('出借金额')->change();
+            $table->decimal('gross_interest', 10, 2)->default(0)->comment('利息总额')->change();
+            $table->decimal('interest_monthly', 10, 2)->default(0)->comment('月付利息')->change();
         });
     }
 
