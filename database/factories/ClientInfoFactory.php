9aa0c0edc8b87537e5769d6bc300d2566c3c868c
Gabriel <obdo.briel@gmail.com>
2017-07-06 14:17:11 +0800

    op client info seeder

--- database/factories/ClientInfoFactory.php
+++ database/factories/ClientInfoFactory.php
@@ -14,21 +14,26 @@
 $factory->define(App\Model\ClientInfo::class, function (Faker\Generator $faker) {
     $start = 1420041600; //2015-01-01 00:00:00
     $end = 1499356800; //2017-07-07 00:00:00
+
+    $int = mt_rand(0, 9999);
+    $float = mt_rand(0, 9999);
+    $fee = strval($int) . '.' . strval($float);
+
     return [
         'fuyou_account'          => str_random(30),
         'pay_type'               => mt_rand(1, 5),
         'deduct_time'            => date('H:i:s', mt_rand($start, $end)),
         'posno'                  => str_random(10),
-        'fee'                    => mt_rand(0, 9999) + mt_rand(0, 9999) / mt_getrandmax(),
+        'fee'                    => floatval($fee),
         'import_bank'            => str_random('64'),
         'import_account'         => str_random('32'),
         'import_name'            => str_random('16'),
         'export_bank'            => str_random('64'),
         'export_account'         => str_random('32'),
         'export_name'            => str_random('16'),
-        'area_id'                => mt_rand(1, 999),
+        'area_id'                => mt_rand(1, 255),
         'area_name'              => str_random('16'),
-        'city_id'                => mt_rand(1, 999),
+        'city_id'                => mt_rand(1, 255),
         'city_name'              => str_random('16'),
         'section'                => str_random('32'),
         'director'               => str_random('16'),
@@ -38,9 +43,9 @@ $factory->define(App\Model\ClientInfo::class, function (Faker\Generator $faker) 
         'team_manager'           => str_random('16'),
         'account_manager'        => str_random('16'),
         'account_manager_cardid' => str_random('18'),
-        'status'                 => mt_rand(1, 999),
-        'deleted_at'             => mt_rand(1000000000, 9999999999),
-        'created_at'             => mt_rand(1000000000, 9999999999),
-        'updated_at'             => mt_rand(1000000000, 9999999999)
+        'status'                 => mt_rand(1, 9),
+        'deleted_at'             => mt_rand($start, $end),
+        'created_at'             => mt_rand($start, $end),
+        'updated_at'             => mt_rand($start, $end)
     ];
 });
