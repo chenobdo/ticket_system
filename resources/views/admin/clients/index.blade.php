d483b549bdd5410ff0dc6e581cbf95bd2482062e
Gabriel <obdo.briel@gmail.com>
2017-07-07 11:14:11 +0800

    add

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -24,6 +24,9 @@
                 <div class="box-header with-border">
                     <h3 class="box-title">客户</h3>
                     <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool">
+                            <i class="fa fa-file"></i>导入
+                        </button>
                         <button type="button" class="btn btn-box-tool" data-widget="collapse">
                             <i class="fa fa-minus"></i>
                         </button>
@@ -35,9 +38,12 @@
                         <table class="table table-hover" id="clienttable">
                             <thead>
                             <tr>
+                                <th>checkbox</th>
                                 <th>合同编号</th>
                                 <th>出借人姓名</th>
                                 <th>出借金额</th>
+                                <th>导入时间</th>
+                                <th>操作</th>
                             </tr>
                             </thead>
                             <tbody></tbody>
@@ -49,47 +55,83 @@
         </section>
     </div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form role="form" method="POST" action="{{ route('clients.store') }}"  enctype="multipart/form-data" class="form-horizontal">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">导入</h4>
+                    </div>
+                    <div class="modal-body">
+                        <label for="gravatar" class="col-sm-2 control-label"></label>
+                        <div class="col-sm-4">
+                            <input type="file" name="file_name" id="file_name">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-purple"><i class="fa fa-pencil"></i>上传Excel</button>
+                    </div>
+                    {!! csrf_field() !!}
+                </form>
+            </div>
+        </div>
+    </div>
+
     @push('scripts')
     <script>
         $(function () {
             var ClientsShowUrl = '{{ url("admin/clients/") }}';
 
-            var ct = $('#clienttable').DataTable({
+            var ct = $("#clienttable").DataTable({
                 columns: [
-                    { data: 'contractno', name: 'contractno' },
-                    { data: 'client', name: 'client' },
-                    { data: 'loan_amount', name: 'loan_amount' }
-//                    { data: null }
+                    { data: null},
+                    { data: "contractno", name: "contractno" },
+                    { data: "client", name: "client" },
+                    { data: "loan_amount", name: "loan_amount" },
+                    { data: "created_at", name: "created_at" },
+                    { data: null}
                 ],
-//                columnDefs: [{
-//                    searchable: false,
-//                    orderable: false,
-//                    targets: 3
-//                }],
-                order: [[0, 'desc']],
+                columnDefs: [
+                    {
+                        targets: 0,
+                        render: function(data, type, row, meta) {
+                            return '<input type="checkbox" name="checklist" value="' + row.id + '" />'
+                        }
+                    },
+                    {
+                        targets: 2,
+                        searchable: false,
+                        orderable: false,
+                        render: function(data, type, row, meta) {
+                            return '<a href="'+ ClientsShowUrl + '/' +row.id +'">'+row.client+'</a>'
+                        }
+                    },
+                    {
+                        targets: -1,
+                        searchable: false,
+                        orderable: false,
+                        render: function(data, type, row, meta) {
+                            return '<a type="button" href="#" onclick="del("'+row.id+'","'+row.contractno+'")">删除</a>';
+                        }
+                    }
+                ],
+                formatNumber: function ( toFormat ) {
+                    return toFormat.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
+                },
+                order: [[4, "desc"]],
                 language: {url: "{{ load_asset('plugins/datatables/localisation/Chinese.json') }}"},
                 processing: true,
                 serverSide: true,
-                ajax: {
-                    url: '{!! route('clients.data') !!}',
-                    dataSrc: function(json) {
-                        $.each(json.data, function(k, v) {
-                            v.client = '<a href="'+ ClientsShowUrl + '/' +v.id +'">'+v.client+'</a>';
-                        });
-                        return json.data;
-                    }
-                },
+                ajax: '{!! route("clients.data") !!}',
                 lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "全部"]]
             });
 
-//            ct.on('order.dt search.dt',function() {
-//                ct.column(3, {
-//                    search: 'applied',
-//                    order: 'applied'
-//                }).nodes().each(function(cell, i) {
-//                    cell.innerHTML = '<button type="button" class="btn btn-box-tool">操作</button>';
-//                });
-//            }).draw();
+            function del(id, contractno) {
+                alert(id + ' ' + contractno);
+            }
         });
     </script>
     @endpush
