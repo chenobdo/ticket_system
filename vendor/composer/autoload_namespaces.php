7ff528364e7352dcb976f4ba88a0607fd062303a
Gabriel <obdo.briel@gmail.com>
2017-07-10 18:47:48 +0800

    add phf

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -9,6 +9,8 @@ return array(
     'tmhOAuth' => array($vendorDir . '/themattharris/tmhoauth'),
     'Tumblr' => array($vendorDir . '/jaapz/tumblr/src'),
     'Thujohn\\Twitter' => array($vendorDir . '/thujohn/twitter/src'),
+    'Svg\\' => array($vendorDir . '/phenx/php-svg-lib/src'),
+    'Sabberworm\\CSS' => array($vendorDir . '/sabberworm/php-css-parser/lib'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'PHPExcel' => array($vendorDir . '/phpoffice/phpexcel/Classes'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
