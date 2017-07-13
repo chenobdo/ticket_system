6cfa508e1ad875afed79e63d805ac47fb8dad0f5
Gabriel <obdo.briel@gmail.com>
2017-07-13 11:43:04 +0800

    pdf test

--- app/Http/Controllers/AdminCheckController.php
+++ app/Http/Controllers/AdminCheckController.php
@@ -22,14 +22,29 @@ class AdminCheckController extends Controller
         $clients = Client::whereIn('contractno', $contractnos)->get();
 
         foreach ($clients as $client) {
+            $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+            $pdf->SetCreator(PDF_CREATOR);
+            $pdf->SetTitle('TCPDF Example 006');
+            $pdf->setPrintHeader(false);
+            $pdf->setPrintFooter(false);
+
+            $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+            $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+            $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+            $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+            $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+            $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+            if (@file_exists(base_path('vendor/tecnickcom/tcpdf/examples/lang/eng.php'))){
+                require_once(base_path('vendor/tecnickcom/tcpdf/examples/lang/eng.php'));
+                $pdf->setLanguageArray($l);
+            }
+            $pdf->SetFont('droidsansfallback', '', 10);
+            $pdf->AddPage();
+
             $html = view('admin.check.template', ['client' => $client])->__toString();
-            $pdf = App::make('dompdf.wrapper');
-            $pdf->loadHTML($html);
-            $filepath = storage_path('app/pdf/');
-            $filename = $client->contractno.$client->client.'.pdf';
-//            $rt = $pdf->save($filepath.$filename);
-//            dd($rt);
-            return $pdf->stream();
+
+            $pdf->writeHTML($html, true, false, true, false, '');
+            $pdf->Output('example_006.pdf', 'I');
         }
     }
 }
