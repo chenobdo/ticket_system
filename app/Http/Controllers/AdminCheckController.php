e03e9da315efaf8b7673eddd207ad5b5afe45465
Gabriel <obdo.briel@gmail.com>
2017-07-13 14:37:06 +0800

    package & download demo

--- app/Http/Controllers/AdminCheckController.php
+++ app/Http/Controllers/AdminCheckController.php
@@ -20,12 +20,40 @@ class AdminCheckController extends Controller
         $contractnos = $request->input('contractnos');
         $clients = Client::whereIn('contractno', $contractnos)->get();
 
+        $pdfdir = storage_path('pdf/'.date('YmdHis').'/');
         foreach ($clients as $client) {
+            $filename = $client->contractno.'.pdf';
+
             $html = view('admin.check.template', ['client' => $client])->__toString();
             $pdf = App::make('snappy.pdf.wrapper');
-            $path = base_path();
-            $pdf->loadHTML($html)->save(storage_path('myfile.pdf'));
-            return $pdf->inline();
+            $filepath = $pdfdir.$filename;
+            $pdf->loadHTML($html)->save($filepath);
         }
+
+        $zip = new ZipArchive();
+        $zippath = storage_path('zip/'.date('YmdHis').'.zip');
+        if ($zip->open($zippath, ZipArchive::OVERWRITE) === TRUE) {
+            $this->zip($pdfdir, $zip);
+            $zip->close();
+        }
+
+        return response()->download($zippath);
+
+        return redirect()->route('clients.index')->with('success', "客户打包成功");;
+    }
+
+    private function zip($path, $zip)
+    {
+        $handler = opendir($path);
+        while (($filename = readdir($handler)) !== false) {
+            if ($filename != "." && $filename != "..") {
+                if (is_dir($path . "/" . $filename)) {
+                    $this->zip($path . "/" . $filename, $zip);
+                } else {
+                    $zip->addFile($path . "/" . $filename);
+                }
+            }
+        }
+        @closedir($path);
     }
 }
